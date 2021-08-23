a0725dbdcfe18b7723263b878c45ad5c484a9825
higovnetwah <wuanhui@govnet.com.cn>
2021-08-23 23:26:37 +0800

    feat: 新增mmall商城项目用户模块

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -81,9 +81,6 @@ public class UserController {
         if(user != null){
             return ServerResponse.createBySuccess(user);
         }
-        if(user != null){
-            return ServerResponse.createBySuccess(user);
-        }
         return ServerResponse.createByErrorMessage("用户未登录,无法获取当前用户的信息");
     }
 
---
+++ src/main/java/com/mmall/pojo/TestPojo.java
@@ -0,0 +1,29 @@
+package com.mmall.pojo;
+
+/**
+ * @ClassName TestPojo
+ * @Description TODO
+ * @Author wah
+ * @Date 2021/8/17/017 21:02
+ * @Version 1.0
+ */
+public class TestPojo {
+    private String name;
+    private Integer id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
